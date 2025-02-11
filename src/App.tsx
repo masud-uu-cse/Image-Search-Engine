3e92290ac9a47eecc90cbf20444c17fa7ea77bcf
masud-uu-cse <2231081073@uttarauniversity.edu.bd>
2025-02-12 02:26:29 +0600

    update App component with new title and description for Image Search Engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 
 function App() {
- 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div style={{ padding: "24px", textAlign: "center" }}>
+      <h1>Image Search Engine </h1>
+      <p>
+        Welcome to the **Image Search Engine**! This app allows you to search
+        for high-quality images using third-party APIs like **Unsplash**,
+        **Pexels**, and **Pixabay**. Whether you're looking for inspiration,
+        stock photos, or just browsing, this app has you covered.
+      </p>
+    </div>
   );
 }
 
